b3d302ecb521fb52d5073a20db42cceecfd96184
DuchateOtaku <gabrielgaonas@outlook.com>
2022-06-26 16:56:00 -0500

    mi primer commit

---
+++ codigo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class codigo {
+    public void kaku(){
+        System.out.println("hola como estas?");
+        Scanner consola = new Scanner(System.in);
+
+    }
+}
